78e4e6705596fe44aa4be17b5421691df8aa700f
Ravi <quonquer@gmail.com>
2025-07-07 08:49:44 +0530

    Fix iframe props and style for Supademo embed (remove non-standard props, fix style object)

--- app/page.tsx
+++ app/page.tsx
@@ -120,9 +120,11 @@ export default function HomePage() {
                   Start Coordinating
                 </button>
               )}
-              <button className="btn-secondary text-lg px-8 py-3">
-                Watch Demo
-              </button>
+              <div style={{ width: '100%', maxWidth: 600 }}>
+                <div style={{ position: 'relative', boxSizing: 'content-box', maxHeight: '80vh', width: '100%', aspectRatio: '1.8274111675126903', padding: '40px 0 40px 0' }}>
+                  <iframe src="https://app.supademo.com/embed/cmcsfqg1t0bv99st8soigyd1b?embed_v=2" loading="lazy" title="Tether Demo" allow="clipboard-write" frameBorder="0" allowFullScreen style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}></iframe>
+                </div>
+              </div>
             </motion.div>
           </div>
         </div>
